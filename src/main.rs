1ec9b8807b16f42fda1acb6e179dda242a16894e
burlinchen <chenbeiliuhuo@gmail.com>
2024-09-22 15:19:29 +0800

    feat: add RISC-V assembly generation and update IR builder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ast_def;
 mod ir_builder;
+mod assembly_builder;
 use ir_builder::{generate_ir};
 use lalrpop_util::lalrpop_mod;
 use std::env::args;
@@ -23,9 +24,26 @@ fn main() -> Result<()> {
   let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
   // 输出解析得到的 AST
   println!("AST:\n{:#?}", ast);
-  let ir: koopa::ir::Program = generate_ir(&ast).expect("IR builder error");
-  let mut text_generator = KoopaGenerator::new(Vec::new());
-  text_generator.generate_on(&ir).unwrap();
-  std::fs::write(output, text_generator.writer()).expect("Unable to write");
+
+  // Generate in-memory Koopa IR (struct Program) using my IR builder.
+  let ir: koopa::ir::Program = ir_builder::generate_ir(&ast).expect("IR builder error");
+  match mode.as_str() {
+    // Convert in-memory Koopa IR to text, and write it to output file (hello.koopa).
+    "-koopa" => {
+      let mut text_generator = KoopaGenerator::new(Vec::new());
+      text_generator.generate_on(&ir).unwrap();
+      std::fs::write(output, text_generator.writer())?;
+      Ok(())
+    }
+    "-riscv" => {
+      let assembly_codes = assembly_builder::generate_assembly(&ir).expect("Assembly builder error");
+      let mut output_file = std::fs::File::create(output)?;
+      for assembly_code in assembly_codes {
+        writeln!(output_file, "{}", assembly_code)?;
+      }
+      Ok(())
+    }
+    mode => Err(mode),
+  }.expect("Wrong mode");
   Ok(())
 }
